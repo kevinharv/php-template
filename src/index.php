518d55b43de3a0020944dbc8b42646b75962dee9
kevinharv <kharvey2002@gmail.com>
2024-02-01 18:25:58 -0600

    feat: re-created and tested basic MySQL connection

--- src/index.php
+++ src/index.php
@@ -5,14 +5,25 @@ $name = "";
 $servername = "mysql";
 $username = "php";
 $password = "other_more_different_gibberish";
+$dbname = "php";
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=php", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["fname"]. "<br>";
+  }
+} else {
+  echo "0 results";
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
